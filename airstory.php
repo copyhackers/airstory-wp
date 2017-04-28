03ce37b4c1d5a0df7d153dc3dc2bdfd6fba9a17e
Steve Grunwell <stevegrunwell@gmail.com>
2017-04-28 16:05:31 -0400

    Move the async tasks into a separate file, instead of the main plugin file

--- airstory.php
+++ airstory.php
@@ -15,23 +15,8 @@ namespace Airstory;
 
 define( 'AIRSTORY_INC', __DIR__ . '/includes' );
 
+require_once AIRSTORY_INC . '/async-tasks.php';
 require_once AIRSTORY_INC . '/class-api.php';
 require_once AIRSTORY_INC . '/core.php';
 require_once AIRSTORY_INC . '/formatting.php';
 require_once AIRSTORY_INC . '/webhook.php';
-
-/**
- * Load asynchronous tasks.
- *
- * To prevent locking the main thread and possibly hitting timeouts, the plugin leverages the
- * TechCrunch WP Asynchronous Tasks library. Actions that require some potentially expensive
- * processes (for instance, side-loading images) can instead be handled asynchronously, by using
- * the wp_async_{hook} pattern.
- *
- * @link https://github.com/techcrunch/wp-async-task
- */
-require_once AIRSTORY_INC . '/lib/wp-async-task/wp-async-task.php';
-require_once AIRSTORY_INC . '/async-tasks/import-post.php';
-
-// Each task must be instantiated once.
-new AsyncTasks\ImportPost();

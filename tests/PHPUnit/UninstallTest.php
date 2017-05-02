32d9477da59feb477619befd72c7fb223d91f877
Steve Grunwell <stevegrunwell@gmail.com>
2017-05-02 13:18:07 -0400

    Call Connection\remove_connection() for each connected user before clearing out the Airstory-related user meta on uninstall

--- tests/PHPUnit/UninstallTest.php
+++ tests/PHPUnit/UninstallTest.php
@@ -5,7 +5,7 @@
  * @package Airstory
  */
 
-namespace Airstory\Core;
+namespace Airstory;
 
 use WP_Mock as M;
 use Mockery;
@@ -20,6 +20,9 @@ class UninstallTest extends \Airstory\TestCase {
 
 		$wpdb = Mockery::mock( 'WPDB' )->makePartial();
 		$wpdb->usermeta = 'my_table';
+		$wpdb->shouldReceive( 'get_col' )
+			->times( 2 )
+			->andReturn( array( 1, 2, 3 ), array() );
 		$wpdb->shouldReceive( 'query' )
 			->once()
 			->andReturnUsing( function ( $query ) {
@@ -36,6 +39,34 @@ class UninstallTest extends \Airstory\TestCase {
 					}
 				}
 			} );
+
+		M::userFunction( __NAMESPACE__ . '\Connection\remove_connection', array(
+			'times' => 3,
+		) );
+
+		define( 'WP_UNINSTALL_PLUGIN', true );
+
+		include PROJECT_ROOT . '/uninstall.php';
+	}
+
+	/**
+	 * If we're chunking the query query and have 150 users connected to Airstory (IDs 1-150,
+	 * conveniently), the query should be run three times: users 1-100, 101-150, then a final run to
+	 * ensure there aren't any left.
+	 */
+	public function testUninstallChunksConnections() {
+		global $wpdb;
+
+		$wpdb = Mockery::mock( 'WPDB' )->makePartial();
+		$wpdb->usermeta = 'my_table';
+		$wpdb->shouldReceive( 'get_col' )
+			->times( 3 )
+			->andReturn( range( 1, 100 ), range( 101, 150 ), array() );
+		$wpdb->shouldReceive( 'query' )->once();
+
+		M::userFunction( __NAMESPACE__ . '\Connection\remove_connection', array(
+			'times' => 150,
+		) );
 
 		define( 'WP_UNINSTALL_PLUGIN', true );
 
--- uninstall.php
+++ uninstall.php
@@ -5,16 +5,36 @@
  * Upon removing the plugin, all related user meta should be cleaned up, but *post* meta should be
  * left intact.
  *
- * @todo Remove any connections within Airstory before the user meta is deleted.
- *
  * @package Airstory
  */
+
+namespace Airstory;
 
 global $wpdb;
 
 // Prevent this file from being executed outside of the plugin uninstallation.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	return;
+}
+
+/**
+ * Collect any users that are connected to Airstory and close their connections.
+ *
+ * To avoid possible cache collisions, this is being written as a direct SQL query.
+ */
+$connected_user_query = "SELECT user_id FROM $wpdb->usermeta WHERE meta_key = '_airstory_target' LIMIT 100";
+$connected_user_ids   = $wpdb->get_col( $connected_user_query ); // WPCS: unprepared SQL ok.
+
+while ( ! empty( $connected_user_ids ) ) {
+	$user_id = array_shift( $connected_user_ids );
+
+	// Remove the user's connection within Airstory.
+	Connection\remove_connection( $user_id );
+
+	// When we get down to 0 entries in the array, run the query again and see if we have more.
+	if ( 0 === count( $connected_user_ids ) ) {
+		$connected_user_ids = $wpdb->get_col( $connected_user_query ); // WPCS: unprepared SQL ok.
+	}
 }
 
 // Remove all known user meta keys.
